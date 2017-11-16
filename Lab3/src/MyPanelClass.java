b45fb35721ab53ffb9cf1949944fa63969e767a0
ivan.alvarado2 <ivan.alvarado2@INCADELWS18.ecenet.ece.uprm.edu>
2017-11-16 09:56:31 -0400

    polygon added, oval commented

---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,76 @@
+	import java.awt.Color;
+	import java.awt.Graphics;
+	import java.awt.Insets;
+import java.awt.Polygon;
+
+import javax.swing.JPanel;
+	 
+	public class MyPanelClass extends JPanel {
+	            /**
+		 * 
+		 */
+		private static final long serialVersionUID = 7057541440811488699L;
+
+				public void paintComponent(Graphics g) {
+	                        super.paintComponent(g);
+	 
+	                        //Compute interior coordinates
+	                        Insets myInsets = this.getInsets();
+	                        int x1 = myInsets.left;
+	                        int y1 = myInsets.top;
+	                        int x2 = getWidth() - myInsets.right - 1;
+	                        int y2 = getHeight() - myInsets.bottom - 1;
+	                        int width = x2 - x1;
+	                        int height = y2 - y1;
+	 
+	                        //Paint the background
+
+	                        g.setColor(Color.RED);
+
+	                        g.fillRect(x1, y1, width+1, height+1);
+//
+//	                        //Draw a border
+//
+//	                        g.setColor(Color.GREEN);
+//
+//	                        g.drawRect(x1, y1, width, height);
+//
+//	                        //Draw Second Border
+//
+//	                        g.setColor(Color.BLACK);
+//
+//	                        g.drawRect(x1+5, y1+5, width-10, height-10);
+//	                        
+//	                        //Draw Diagonal Line
+//	                        
+//	                        g.setColor(Color.WHITE);
+//
+//	                        g.drawLine(x1+115, y1, x2, y2);
+//
+//	                       //Draw Opposite Diagonal Line
+//	                        
+//	                        g.setColor(Color.BLUE);
+//
+//	                        g.drawLine(x1, y2, x2, y1);
+//	                        
+//	                        //Draw Oval
+//	                        g.setColor(Color.PINK);
+//	                        g.fillOval((width-55)/2, ((height-55)/2),55,55);
+	                        
+	                        Polygon p2 = new Polygon();
+	                        p2.addPoint(x1 + 25, y1 + 73);
+	                        p2.addPoint(x1 + 41, y1 + 73);
+	                        p2.addPoint(x1 + 47, y1 + 58);
+	                        p2.addPoint(x1 + 53, y1 + 73);
+	                        p2.addPoint(x1 + 69, y1 + 73);
+	                        p2.addPoint(x1 + 56, y1 + 83);
+	                        p2.addPoint(x1 + 61, y1 + 98);
+	                        p2.addPoint(x1 + 47, y1 + 88);
+	                        p2.addPoint(x1 + 34, y1 + 98);
+	                        p2.addPoint(x1 + 38, y1 + 83);
+	                        	                        
+	                        g.setColor(Color.WHITE);
+	                        g.drawPolygon(p2);
+	            }
+
+	}
